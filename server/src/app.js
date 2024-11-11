0a7f702215c4973a5c5d28b5a6504181f0c95bd3
Maksym Matsuhyria <maksymm@student.chalmers.se>
2024-11-11 11:32:37 +0100

    add initial project structure

---
+++ server/src/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const morgan = require('morgan');
+const path = require('path');
+const cors = require("cors");
+const connectDB = require("../config/db");
+
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:3100/dentist_db';
+const PORT = process.env.PORT || 5000;
+
+
+connectDB(MONGODB_URI);
+
+const app = express();
+app.use(express.json());
+app.use(require("cors"));
+app.use(morgan('dev'));
+app.options('*', cors());
+app.use(cors());
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
