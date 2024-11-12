46fb2f6205590ae3d2a9c1c2491848cb1ee41f88
Maksym Matsuhyria <maksymm@student.chalmers.se>
2024-11-12 17:01:02 +0100

    add mocha tests

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const connectDB = async (URIpath) => {
     try {
@@ -10,4 +10,4 @@ const connectDB = async (URIpath) => {
     }
 };
 
-module.exports = connectDB;
+export default connectDB;
