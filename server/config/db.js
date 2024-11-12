0e12ae2cac655a47f3a1413a522d16b2f955b9c1
Maksym Matsuhyria <maksymm@student.chalmers.se>
2024-11-12 15:12:41 +0100

    add ci/cd pipeline for server

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,11 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const connectDB = async (URIpath) => {
     try {
         await mongoose.connect(URIpath);
-        console.log("MongoDB connected");
+        console.log('MongoDB connected');
     } catch (error) {
-        console.error("MongoDB connection error: ", error);
+        console.error('MongoDB connection error: ', error);
         process.exit(1);
     }
 };
--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -5,18 +5,15 @@ import pluginJs from '@eslint/js';
 /** @type {import('eslint').Linter.Config[]} */
 export default [
     { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
-    { languageOptions: { globals: globals.browser } },
+    { languageOptions: { globals: globals.node } },
     pluginJs.configs.recommended,
     {
         rules: {
-            "env": ["warn", {
-                "node": true
-            }],
             'no-unused-vars': 'warn',
-            'no-console': 'warn',
             'semi': ['error', 'always'],
             'quotes': ['error', 'single'],
             'indent': ['error', 4],
+            'no-trailing-spaces': 'error'
         }
     }
 ];
