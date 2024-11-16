982f44aeaf52bca5a61a9089959ee82237e195a9
Ahmet Yavuz Kalkan <ahmety@student.chalmers.se>
2024-11-16 16:49:15 +0100

    Feat-In Progress Create TimeSlot&Table Components #2
    The components work together to create a time table where users can view possible time slots for an appointment
    
    Components will be further modified to implement booking, and have a fitting look for the rest of the application. Additionally, the findAppointment method in WeeklyTimeTable component could be changed with something more efficient

---
+++ client/src/components/TimeSlot.vue
@@ -0,0 +1,52 @@
+<template>
+  <div :class="slotClass" @click="handleClick">
+    {{ this.slotStartingHour }}:00 - {{ this.slotStartingHour + 1 }}:00
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TimeSlot',
+  props: {
+    slotStartingHour: Number,
+    appointment: Object,
+  },
+  computed: {
+    slotClass() {
+      if (this.appointment) {
+        if (this.appointment.status === 'booked') {
+          // Booked by you or else logic could be added here
+          return 'booked';
+        } else if (this.appointment.status === 'canceled') {
+          // Don't know what cancelled status means
+          return 'available';
+        }
+        return 'available';
+      }
+      return 'unavailable';
+    }
+  },
+  methods: {
+    handleClick() {
+      if (this.slotClass === 'available') {
+        // Create a separate component for book handling as this
+        //component would also show the data that is accessible to the patient
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+.available {
+  background-color: white;
+}
+
+.booked {
+  background-color: red;
+}
+
+.unavailable {
+  background-color: gray;
+}
+</style>
