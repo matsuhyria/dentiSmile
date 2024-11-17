20937c2b19fd7e8f39eb20a3171761c6f08348af
Yaroslav Ursul <ursul@student.chalmers.se>
2024-11-17 22:35:00 +0000

    Revert "Merge branch 'main' of git.chalmers.se:courses/dit355/2024/student_teams/dit356_2024_07/group-7"
    This reverts commit 771578bdcdc4d9fcf5d55157699ccaccf69b162c

--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,13 @@
-import mongoose from 'mongoose';
+const mongoose = require("mongoose");
 
 const connectDB = async (URIpath) => {
     try {
         await mongoose.connect(URIpath);
-        console.log('MongoDB connected');
+        console.log("MongoDB connected");
     } catch (error) {
-        console.error('MongoDB connection error: ', error);
+        console.error("MongoDB connection error: ", error);
         process.exit(1);
     }
 };
 
-export default connectDB;
+module.exports = connectDB;
