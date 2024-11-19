b0d4edf662311d829456ee7f1db7436e7b8ccf58
Yaroslav Ursul <gusursya@student.gu.se>
2024-11-19 10:31:33 +0100

    #3 Implement patient registration and login

--- server/patient-service/src/controllers/authController.js
+++ server/patient-service/src/controllers/authController.js
@@ -1,1 +1,43 @@
-import Dentist from '../models/patient.js';
+import Patient from '../models/patient.js';
+import bcrypt from 'bcrypt';
+
+const register = async (req, res) => {
+    const { username, password, phone, email, age } = req.body;
+
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newPatient = new Patient({
+            username,
+            password: hashedPassword,
+            phone,
+            age,
+            email
+        });
+
+        await newPatient.save();
+        res.status(201).json({ message: 'Patient registered successfully' });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'Error registering patient' });
+    }
+};
+
+const login = async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const patient = await Patient.findOne({ username });
+        if (!patient) return res.status(404).json({ message: 'Patient not found' });
+
+        const isMatch = await bcrypt.compare(password, patient.password);
+        if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
+
+        // TO-DO add token generation
+        res.status(200).json({ message: 'Login successful' });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'Error logging in' });
+    }
+};
+
+export { register, login };
--- server/patient-service/src/models/patient.js
+++ server/patient-service/src/models/patient.js
@@ -1,7 +1,28 @@
 import mongoose from 'mongoose';
 
 const patientSchema = new mongoose.Schema({
-    
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    age: {
+        type: Number,
+        required: true,
+    },
+    phoneNumber: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
 });
 
 const Patient = mongoose.model('Patient', patientSchema);
