70c55ba85f972fc6d0bfc9f497a039ba537cd3cf
Maksym Matsuhyria <maksymm@student.chalmers.se>
2024-12-02 13:49:42 +0100

    #9 refine login

--- server/auth_service/src/routes/authRoutes.js
+++ server/auth_service/src/routes/authRoutes.js
@@ -1,8 +1,11 @@
 import mqttUtils from 'shared-mqtt';
-import { register } from '../controllers/authController.js';
+import { register, login } from '../controllers/authController.js';
 
 const { handleEndpoint, MQTT_TOPICS } = mqttUtils;
 
 export const initializeRoutes = async () => {
-    await handleEndpoint(MQTT_TOPICS.AUTHENTICATION.REGISTER.REQUEST, register, MQTT_TOPICS.AUTHENTICATION.REGISTER.RESPONSE);
+    const { REGISTER, LOGIN } = MQTT_TOPICS.AUTHENTICATION;
+
+    await handleEndpoint(REGISTER.REQUEST, register, REGISTER.RESPONSE);
+    await handleEndpoint(LOGIN.REQUEST, login, LOGIN.RESPONSE);
 };
