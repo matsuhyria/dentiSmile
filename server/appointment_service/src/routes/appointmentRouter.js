f1ca66c5f643cd646b515bd63c642836242ee344
Ahmet Yavuz Kalkan <ahmety@student.chalmers.se>
2024-12-07 12:35:01 +0100

    #14 Catch errors during publishing

--- server/appointment_service/src/routes/appointmentRouter.js
+++ server/appointment_service/src/routes/appointmentRouter.js
@@ -9,34 +9,56 @@ const mqttRouter = async () => {
         Dentist Related Topics
     */
 
-   // Get Appointments for Dentist
-   await subscribe(MQTT_TOPICS.DENTIST.GET_APPOINTMENTS.REQUEST, async (message) => {
-       console.log('Get Appointments Request:', message);
-       publish(MQTT_TOPICS.DENTIST.GET_APPOINTMENTS.RESPONSE(message.clientId), await getAppointments(message))
-   })
-    
+    // Get Appointments for Dentist
+    await subscribe(MQTT_TOPICS.DENTIST.GET_APPOINTMENTS.REQUEST, async (message) => {
+        try {
+            console.log('Get Appointments Request:', message);
+            publish(MQTT_TOPICS.DENTIST.GET_APPOINTMENTS.RESPONSE(message.clientId), await getAppointments(message))
+        } catch (error) {
+            console.error('Error caught during publishing: ', error);
+        }
+    })
+
     // Get Appointment for Dentist
     await subscribe(MQTT_TOPICS.DENTIST.GET_APPOINTMENT.REQUEST, async (message) => {
-        console.log('Get Appointment Request:', message);
-        publish(MQTT_TOPICS.DENTIST.GET_APPOINTMENT.RESPONSE(message.clientId), await getSlotDetails(message))
+        try {
+            console.log('Get Appointment Request:', message);
+            publish(MQTT_TOPICS.DENTIST.GET_APPOINTMENT.RESPONSE(message.clientId), await getSlotDetails(message))
+        } catch (error) {
+            console.error('Error caught during publishing: ', error);
+        }
     })
-    
+
     // Create Appointments for Dentist
     await subscribe(MQTT_TOPICS.DENTIST.REGISTER_AVAILABILITY.REQUEST, async (message) => {
-        console.log('Create Appointments Request:', message);
-        publish(MQTT_TOPICS.DENTIST.REGISTER_AVAILABILITY.RESPONSE(message.clientId), await createAppointments(message))
+        try {
+            console.log('Create Appointments Request:', message);
+            publish(MQTT_TOPICS.DENTIST.REGISTER_AVAILABILITY.RESPONSE(message.clientId), await createAppointments(message))
+        } catch (error) {
+            console.error('Error caught during publishing: ', error);
+        }
     })
-    
+
     // Cancel Appointment for Dentist
     await subscribe(MQTT_TOPICS.DENTIST.CANCEL_APPOINTMENT.REQUEST, async (message) => {
-        console.log('Cancel Appointment Request:', message);
-        publish(MQTT_TOPICS.DENTIST.CANCEL_APPOINTMENT.RESPONSE(message.clientId), await cancelAppointment(message))
+        try {
+            console.log('Cancel Appointment Request:', message);
+            publish(MQTT_TOPICS.DENTIST.CANCEL_APPOINTMENT.RESPONSE(message.clientId), await cancelAppointment(message))
+        }
+        catch (error) {
+            console.error('Error caught during publishing: ', error);
+        }
     })
-    
+
     // Remove Appointment for Dentist
     await subscribe(MQTT_TOPICS.DENTIST.REMOVE_APPOINTMENT.REQUEST, async (message) => {
-        console.log('Remove Appointment Request:', message);
-        publish(MQTT_TOPICS.DENTIST.REMOVE_APPOINTMENT.RESPONSE(message.clientId), await removeAppointment(message))
+        try {
+            console.log('Remove Appointment Request:', message);
+            publish(MQTT_TOPICS.DENTIST.REMOVE_APPOINTMENT.RESPONSE(message.clientId), await removeAppointment(message))
+        }
+        catch (error) {
+            console.error('Error caught during publishing: ', error);
+        }
     })
 
     /* 
@@ -45,14 +67,24 @@ const mqttRouter = async () => {
 
     // Get Appointments for Patient
     await subscribe(MQTT_TOPICS.PATIENT.GET_APPOINTMENTS.REQUEST, async (message) => {
-        console.log('Get Appointments Request:', message);
-        publish(MQTT_TOPICS.PATIENT.GET_APPOINTMENTS.RESPONSE(message.clientId), await getAppointments(message))
+        try {
+            console.log('Get Appointments Request:', message);
+            publish(MQTT_TOPICS.PATIENT.GET_APPOINTMENTS.RESPONSE(message.clientId), await getAppointments(message))
+        }
+        catch (error) {
+            console.error('Error caught during publishing: ', error);
+        }
     })
 
     // Book Appointment for Patient
     await subscribe(MQTT_TOPICS.PATIENT.BOOK_APPOINTMENT.REQUEST, async (message) => {
-        console.log('Book Appointment Request:', message);
-        publish(MQTT_TOPICS.PATIENT.BOOK_APPOINTMENT.RESPONSE(message.clientId), await bookAppointment(message))
+        try {
+            console.log('Book Appointment Request:', message);
+            publish(MQTT_TOPICS.PATIENT.BOOK_APPOINTMENT.RESPONSE(message.clientId), await bookAppointment(message))
+        }
+        catch (error) {
+            console.error('Error caught during publishing: ', error);
+        }
     })
 }
 
