e2faebb73de6fa03490e92b9b48e837afff43492
Yaro <ursul@student.chalmers.se>
2024-12-09 19:53:15 +0100

    #28 Initialize the notification service

---
+++ server/notification_service/src/app.js
@@ -0,0 +1,26 @@
+import connectDB from './config/db.js';
+import mqttUtils from 'shared-mqtt';
+
+const { connectMQTT } = mqttUtils;
+const PORT = process.env.PORT || 3001;
+const MONGODB_URI = process.env.MONGODB_URI;
+const MQTT_URI = process.env.MQTT_URI;
+const MQTT_OPTIONS = {
+    clientId: 'notificationService',
+    reconnectPeriod: 2000,
+    connectTimeout: 30 * 1000,
+    clean: true
+}
+
+const startService = async () => {
+    try {
+        await connectDB(MONGODB_URI);
+        await connectMQTT(MQTT_URI, MQTT_OPTIONS);
+        console.log(`Service is running on port ${PORT}`);
+    } catch (error) {
+        console.error('Error starting the service:', error);
+        process.exit(1);
+    }
+};
+
+await startService();
