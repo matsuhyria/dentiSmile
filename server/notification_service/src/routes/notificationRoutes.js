cc13549b04ead4c8592b94e5326b0cb41d80a179
Yaro <ursul@student.chalmers.se>
2024-12-09 20:06:58 +0100

    #28 Create structure for notification service

---
+++ server/notification_service/src/routes/notificationRoutes.js
@@ -0,0 +1,11 @@
+import mqttUtils from 'shared-mqtt';
+// import { register, login } from '../controllers/autController.js';
+
+const { handleEndpoint, MQTT_TOPICS } = mqttUtils;
+
+export const initializeRoutes = async () => {
+    const { REGISTER, LOGIN } = MQTT_TOPICS.NOTIFICATION;
+
+    await handleEndpoint(REGISTER.REQUEST, register, REGISTER.RESPONSE);
+    await handleEndpoint(LOGIN.REQUEST, login, LOGIN.RESPONSE);
+};
