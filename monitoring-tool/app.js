ff2b12a97d515a95184df5032d456922822a080c
Yaro <ursul@student.chalmers.se>
2024-12-17 20:31:35 +0100

    #41 Refactor the monitoring tool and make use of Event emmiter

--- monitoring-tool/app.js
+++ monitoring-tool/app.js
@@ -2,112 +2,122 @@ import { connectMQTT, subscribe } from '../shared/mqtt/mqttClient.js';
 import mongoose from 'mongoose';
 import { MQTT_TOPICS } from '../shared/mqtt/mqttTopics.js';
 import connectDB from './config/db.js';
+import EventEmitter from 'events';
 
+const metricsEmitter = new EventEmitter();
 
-const MONGODB_URI = process.env.MONGODB_URI;
-const MQTT_URI = process.env.MQTT_URI || 'mqtt://localhost:1883';
-const PORT = process.env.PORT || 3005;
+class MonitoringTool {
+    constructor() {
+        this.metrics = {
+            activeClients: 0,
+            dentistCreate: 0,
+            searchingPatients: 0,
+            registeredDentists: 0,
+            availableAppointments: 0
+        };
 
+        this.topicsToMonitor = [
+            '$SYS/broker/clients/active',
+            MQTT_TOPICS.APPOINTMENT.CREATE.REQUEST,
+            MQTT_TOPICS.APPOINTMENT.RETRIEVE.MANY.REQUEST,
+            MQTT_TOPICS.APPOINTMENT.RETRIEVE.ONE.REQUEST
+        ];
 
-const MQTT_OPTIONS = {
-    clientId: 'monitoringTool',
-    reconnectPeriod: 2000,
-    connectTimeout: 30 * 1000,
-    clean: true
-};
-
-let metrics = {
-    connectedClients: 0,
-    dentistCreate: 0,
-    searchingPatients: 0,
-    registeredDentists: 0,
-    availableAppointments: 0
-
-};
-
-const topicsToMonitor = [
-    '$SYS/broker/clients/active',
-    '$SYS/broker/clients/connected',
-    MQTT_TOPICS.APPOINTMENT.CREATE.REQUEST,
-    MQTT_TOPICS.APPOINTMENT.RETRIEVE.MANY.REQUEST,
-    MQTT_TOPICS.APPOINTMENT.RETRIEVE.ONE.REQUEST
-
-];
-
-const fetchDentistMetrics = async () => {
-    try {
-        const collectionDentists = mongoose.connection.db.collection('dentist');
-        metrics.registeredDentists = await collectionDentists.countDocuments();
-        
-    } catch (error) {
-        console.error('Detailed error in fetchMetricsFromDB:', error)
-        throw error;
+        metricsEmitter.on('metricsUpdated', this.displayMetricsMenu.bind(this));
     }
-};
 
-const fetchSlotMetrics = async () => {
-    try {
-        const collectionSlots = mongoose.connection.db.collection('slots');
-        metrics.availableAppointments = await collectionSlots.countDocuments({status: 'available'});
-        
-    } catch (error) {
-        console.error('Detailed error in fetchMetricsFromDB:', error)
-        throw error;
+    async fetchDentistMetrics() {
+        try {
+            const collectionDentists = mongoose.connection.db.collection('dentist');
+            this.metrics.registeredDentists = await collectionDentists.countDocuments();
+            metricsEmitter.emit('metricsUpdated');
+        } catch (error) {
+            console.error('Error fetching dentist metrics:', error);
+        }
     }
-};
 
-const updateMetrics = (topic, message) => {
-
-    //const value = parseInt(message, 10);
-
-    if (topic === '$SYS/broker/clients/connected'){
-        metrics.connectedClients = value;
-    } 
-    if (topic === '$SYS/broker/clients/disconnected'){
-        metrics.disconnectedClients = value;
+    async fetchSlotMetrics() {
+        try {
+            const collectionSlots = mongoose.connection.db.collection('slots');
+            this.metrics.availableAppointments = await collectionSlots.countDocuments({status: 'available'});
+            metricsEmitter.emit('metricsUpdated');
+        } catch (error) {
+            console.error('Error fetching slot metrics:', error);
+        }
     }
-    if (topic === MQTT_TOPICS.APPOINTMENT.CREATE.REQUEST){
-        metrics.dentistCreate ++;
+
+    updateMetrics(topic, message) {
+        const value = parseInt(message, 10);
+
+        switch(topic) {
+            case '$SYS/broker/clients/active':
+                this.metrics.activeClients = value;
+                break;
+            case MQTT_TOPICS.APPOINTMENT.CREATE.REQUEST:
+                this.metrics.dentistCreate++;
+                break;
+            case MQTT_TOPICS.APPOINTMENT.RETRIEVE.MANY.REQUEST:
+            case MQTT_TOPICS.APPOINTMENT.RETRIEVE.ONE.REQUEST:
+                this.metrics.searchingPatients++;
+                break;
+        }
+
+        metricsEmitter.emit('metricsUpdated');
     }
-    if (topic === MQTT_TOPICS.APPOINTMENT.RETRIEVE.MANY || topic === MQTT_TOPICS.APPOINTMENT.RETRIEVE.ONE){
-        metrics.searchingPatients ++;
+
+    displayMetricsMenu() {
+        console.clear();
+        console.log('==== MQTT Broker Metrics ====');
+        console.log(`Active users                           : ${this.metrics.activeClients}`);
+        console.log(`Dentists publishing new appointments   : ${this.metrics.dentistCreate}`);
+        console.log(`Searching patients                     : ${this.metrics.searchingPatients}`);
+        console.log(`Registered dentists                    : ${this.metrics.registeredDentists}`);
+        console.log(`Available appointments                 : ${this.metrics.availableAppointments}`);
+        console.log('=============================');
+    }
+
+    async start(mongoUri, mqttUri, port) {
+        try {
+            console.log(`Service is running on port ${port}`);
+            await connectDB(mongoUri);
+            await connectMQTT(mqttUri, {
+                clientId: 'monitoringTool',
+                reconnectPeriod: 2000,
+                connectTimeout: 30 * 1000,
+                clean: true
+            });
+
+            // fetch metrics upon the initialization of the tool
+            await this.fetchDentistMetrics();
+            await this.fetchSlotMetrics();
+
+            for (const topic of this.topicsToMonitor) {
+                await subscribe(topic, (message) => {
+                    this.updateMetrics(topic, message);
+                });
+            }
+
+            // database refresh
+            setInterval(() => {
+                this.fetchDentistMetrics();
+                this.fetchSlotMetrics();
+            }, 30000);
+
+            // menu update
+            setInterval(() => {
+                metricsEmitter.emit('metricsUpdated');
+            }, 2000);
+
+        } catch (error) {
+            console.error('Error starting the monitoring service:', error);
+            process.exit(1);
+        }
     }
 }
 
-const displayMetricsMenu = () => {
-    //console.clear(); // Clear the terminal before displaying new data
-    console.log('==== MQTT Broker Metrics ====');
-    console.log(`Connected Users                                      : ${metrics.connectedClients}`);
-    console.log(`The amount of dentists publishing new appointments   : ${metrics.dentistCreate}`);
-    console.log(`The number of registered dentists                    : ${metrics.registeredDentists}`);
-    console.log(`The amount of available appointments                 : ${metrics.availableAppointments}`);
-    console.log('=============================');
-  };
-
-const startTool = async () => {
-    try {
-        console.log(`Service is running on port ${PORT}`);
-        await connectDB(MONGODB_URI);
-        await connectMQTT(MQTT_URI ,MQTT_OPTIONS);
-        
-        setInterval(displayMetricsMenu, 5000);
-
-
-        for (let topic of topicsToMonitor) {
-            await subscribe(topic, (message) => {
-                console.log('!')
-                updateMetrics(topic, message);
-                fetchDentistMetrics();
-                fetchSlotMetrics();
-
-            });
-            
-        }
-    
-    } catch (error) {
-        console.error('Error starting the monitoring service:', error);
-        process.exit(1);
-    }
-};
-
-startTool();
+const monitoringTool = new MonitoringTool();
+monitoringTool.start(
+    process.env.MONGODB_URI || 'mongodb://localhost:27017/appointment_db',
+    process.env.MQTT_URI || 'mqtt://localhost:1883', 
+    process.env.PORT || 3005
+);
