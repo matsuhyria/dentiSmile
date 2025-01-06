e16e1e410b92c860eeffe18493d95c178d7f6f8f
Mahmoud Khalafallah <mahmoudk@student.chalmers.se>
2025-01-06 21:30:05 +0100

    #17 Add lock and unlock functionality to appointment service

--- server/appointment_service/src/routes/appointmentRouter.js
+++ server/appointment_service/src/routes/appointmentRouter.js
@@ -1,4 +1,4 @@
-import { getAppointmentsByClinic, createAppointments, getAppointmentsByDentist, bookAppointment, getAppointmentById, cancelAppointment, removeAppointment, getAppointmentsByPatientId } from '../controllers/appointmentController.js';
+import { getAppointmentsByClinic, createAppointments, getAppointmentsByDentist, bookAppointment, getAppointmentById, cancelAppointment, removeAppointment, lockAppointment, unlockAppointment, getAppointmentsByPatientId } from '../controllers/appointmentController.js';
 import mqttUtils from 'shared-mqtt'
 const { handleEndpoint, MQTT_TOPICS, publish } = mqttUtils;
 
@@ -26,7 +26,7 @@ const publishNotification = async (day) => {
 };
 
 export const initializeRoutes = async () => {
-    const { CREATE, RETRIEVE, BOOK, CANCEL, DELETE, CLINIC, PATIENT } = MQTT_TOPICS.APPOINTMENT;
+    const { CREATE, RETRIEVE, BOOK, CANCEL, DELETE, CLINIC, PATIENT, LOCK, UNLOCK } = MQTT_TOPICS.APPOINTMENT;
 
     await handleEndpoint(CLINIC.RETRIEVE.REQUEST, getAppointmentsByClinic, CLINIC.RETRIEVE.RESPONSE);
     await handleEndpoint(RETRIEVE.MANY.REQUEST, getAppointmentsByDentist, RETRIEVE.MANY.RESPONSE);
@@ -36,4 +36,6 @@ export const initializeRoutes = async () => {
     await handleEndpoint(CANCEL.REQUEST, cancelAppointment, CANCEL.RESPONSE);
     await handleEndpoint(DELETE.REQUEST, removeAppointment, DELETE.RESPONSE);
     await handleEndpoint(BOOK.REQUEST, bookAppointment, BOOK.RESPONSE);
+    await handleEndpoint(LOCK.REQUEST, lockAppointment, LOCK.RESPONSE);
+    await handleEndpoint(UNLOCK.REQUEST, unlockAppointment, UNLOCK.RESPONSE);
 }
